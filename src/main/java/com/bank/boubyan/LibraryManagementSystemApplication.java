7cd44fd08282d50b0ae18c479e3c8e652db5aaed
Âbdallah Šamir <abdallahsameer22@gmail.com>
2024-08-12 14:55:11 +0300

    enhancements

--- src/main/java/com/bank/boubyan/LibraryManagementSystemApplication.java
+++ src/main/java/com/bank/boubyan/LibraryManagementSystemApplication.java
@@ -1,7 +1,5 @@
 package com.bank.boubyan;
 
-import com.bank.boubyan.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
